a1a99ec581061cefbd2123a29a084b7a44448ac1
Mahmoud samy <58609413+MahhmoudSamy@users.noreply.github.com>
2022-10-31 03:55:50 +0200

    Add files via upload

--- home.dart
+++ home.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:homepage/main.dart';
+import 'package:homepage/screens/chatPage.dart';
 
+void main() => runApp(new MyApp());
 
-class HomeTabScreen extends StatelessWidget {
-  const HomeTabScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  PageController pageController = PageController();
+  void onTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    pageController.jumpToPage(index);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Home"));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('GFG'),
+        ),
+        body: ChatPage(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.directions_car), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.navigation), label: 'MyTrips'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble),
+              label: 'Chat',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Info'),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.green,
+          unselectedItemColor: Colors.grey,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+          unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+          onTap: onTapped,
+        ));
   }
 }
