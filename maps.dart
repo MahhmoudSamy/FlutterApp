11ea9c7955f378ec77e4180374aa63cf1bf5930d
yhossamm <102923533+yhossamm@users.noreply.github.com>
2022-10-31 03:45:39 +0200

    Add files via upload

---
+++ maps.dart
@@ -0,0 +1,162 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class FromTo extends StatefulWidget {
+  static const routeName = "/FromTo";
+
+  @override
+  State<FromTo> createState() => MapFromToState();
+}
+
+class MapFromToState extends State<FromTo> {
+  static const routeName = "/from-to";
+  Completer<GoogleMapController> _controller = Completer();
+  TextEditingController _originController = TextEditingController();
+  TextEditingController _destinationController = TextEditingController();
+
+  Set<Marker> _markers = Set<Marker>();
+  Set<Polygon> _polygons = Set<Polygon>();
+  Set<Polyline> _polylines = Set<Polyline>();
+  List<LatLng> polygonLatLngs = <LatLng>[];
+
+  int _polygonIdCounter = 1;
+  int _polylineIdCounter = 1;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(30.033333, 31.233334),
+    zoom: 14.4746,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    _setMarker(LatLng(30.033333, 31.233334));
+  }
+
+  void _setMarker(LatLng point) {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('marker'),
+          position: point,
+        ),
+      );
+    });
+  }
+
+  void _setPolygon() {
+    final String polygonIdVal = 'polygon_$_polygonIdCounter';
+    _polygonIdCounter++;
+
+    _polygons.add(
+      Polygon(
+        polygonId: PolygonId(polygonIdVal),
+        points: polygonLatLngs,
+        strokeWidth: 2,
+        fillColor: Colors.transparent,
+      ),
+    );
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+  
+    
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _originController,
+                      decoration:
+                          InputDecoration(hintText: ' Enter Start Point'),
+                      onChanged: (value) {
+                        print(value);
+                      },
+                    ),
+                    TextFormField(
+                      controller: _destinationController,
+                      decoration: InputDecoration(hintText: 'Enter Drop Point'),
+                      onChanged: (value) {
+                        print(value);
+                      },
+                    ),
+                    ButtonBar(
+                      children: [
+                        //  TextButton(onPressed: () {}, child: Text("Cancle")),
+                        ElevatedButton(
+                          onPressed: () async {
+                            Navigator.pushNamed(context, '/DriverDetails1');
+                          },
+                          child: Text("Confirm"),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            
+            ],
+          ),
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.normal,
+              markers: _markers,
+              polygons: _polygons,
+              polylines: _polylines,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              onTap: (point) {
+                setState(() {
+                  polygonLatLngs.add(point);
+                  _setPolygon();
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _goToPlace(
+    // Map<String, dynamic> place,
+    double lat,
+    double lng,
+    Map<String, dynamic> boundsNe,
+    Map<String, dynamic> boundsSw,
+  ) async {
+    // final double lat = place['geometry']['location']['lat'];
+    // final double lng = place['geometry']['location']['lng'];
+
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(lat, lng), zoom: 12),
+      ),
+    );
+
+    controller.animateCamera(
+      CameraUpdate.newLatLngBounds(
+          LatLngBounds(
+            southwest: LatLng(boundsSw['lat'], boundsSw['lng']),
+            northeast: LatLng(boundsNe['lat'], boundsNe['lng']),
+          ),
+          25),
+    );
+    _setMarker(LatLng(lat, lng));
+  }
+}
